863af60c5fb9fea925cce8b9353728c884174b28
arip995 <arindampanda2002@gmail.com>
2024-06-02 22:33:36 +0530

    updated code

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,9 +8,9 @@ import MyOrders from "./MyOrders";
 import Cart from "./Cart";
 import FeedbackAdmin from "./FeedbackAdmin";
 import PlaceOrder from "./PlaceOrder";
-import CancelOrder from './CancelOrder';
-import UpdateOrderStatus from './UpdateOrderStatus';
-import OrderedFoodItems from './OrderedFoodItems';
+import CancelOrder from "./CancelOrder";
+import UpdateOrderStatus from "./UpdateOrderStatus";
+import OrderedFoodItems from "./OrderedFoodItems";
 import Login from "./Login";
 import AddCustomerDetails from "./AddCustomerDetails";
 import AddCategories from "./AddCategories";
@@ -27,128 +27,158 @@ import FeedBack from "./FeedBack";
 import CustomerSearch from "./CustomerSearch";
 import "./Home.css";
 function Home() {
-    const [usernameError, setUsernameError] = useState('');
-    const [passwordError, setPasswordError] = useState('');
-    const [valid, setValid] = useState(true);
-    const apiservice = new ApiService();
-    const navigate = useNavigate();
-    const [login, setLogin] = useState({});
+	const [usernameError, setUsernameError] = useState("");
+	const [passwordError, setPasswordError] = useState("");
+	const [valid, setValid] = useState(true);
+	const apiservice = new ApiService();
+	const navigate = useNavigate();
+	const [login, setLogin] = useState({});
 
-    const logout = () => {
-        localStorage.removeItem('token');
-        localStorage.removeItem('role');
-        localStorage.removeItem('username');
-        localStorage.removeItem('loginDetails');
-        localStorage.setItem('loggedIn', 'false');
-        navigate('/');
-    };
+	const logout = () => {
+		localStorage.removeItem("token");
+		localStorage.removeItem("role");
+		localStorage.removeItem("username");
+		localStorage.removeItem("loginDetails");
+		localStorage.setItem("loggedIn", "false");
+		navigate("/");
+	};
 
-    const nameHandler = (event) => {
-        setLogin((prevLogin) => ({ ...prevLogin, username: event.target.value }));
-    };
+	const nameHandler = (event) => {
+		setLogin((prevLogin) => ({ ...prevLogin, username: event.target.value }));
+	};
 
-    const passHandler = (event) => {
-        setLogin((prevLogin) => ({ ...prevLogin, password: event.target.value }));
-    };
+	const passHandler = (event) => {
+		setLogin((prevLogin) => ({ ...prevLogin, password: event.target.value }));
+	};
 
-    const validation = () => {
-        if (!login.username) {
-            setValid(false);
-            setUsernameError('Please enter username');
-        }
-        if (!login.password) {
-            setValid(false);
-            setPasswordError('Please enter password');
-        }
-    };
+	const validation = () => {
+		if (!login.username) {
+			setValid(false);
+			setUsernameError("Please enter username");
+		}
+		if (!login.password) {
+			setValid(false);
+			setPasswordError("Please enter password");
+		}
+	};
 
-    const register = () => {
-        navigate('../register');
-    };
+	const register = () => {
+		navigate("../register");
+	};
 
-    const authorize = (event) => {
-        event.preventDefault();
-        validation();
-        if (valid) {
-            apiservice.authenticateLogin(login).then(
-                (res) => {
-                    localStorage.setItem('token', res.data.token);
-                    localStorage.setItem('role', res.data.role);
-                    localStorage.setItem('user', login.username);
-                    localStorage.setItem('loggedIn', 'true');
-                    alert('Logged In Successfully');
-                    navigate('../menu');
-                },
-                (err) => {
-                    alert('Login Unsuccessful');
-                    navigate('/');
-                }
-            );
-        }
-    };
+	const authorize = (event) => {
+		event.preventDefault();
+		validation();
+		if (valid) {
+			apiservice.authenticateLogin(login).then(
+				(res) => {
+					localStorage.setItem("token", res.data.token);
+					localStorage.setItem("role", res.data.role);
+					localStorage.setItem("user", login.username);
+					localStorage.setItem("loggedIn", "true");
+					alert("Logged In Successfully");
+					navigate("../menu");
+				},
+				(err) => {
+					alert("Login Unsuccessful");
+					navigate("/");
+				},
+			);
+		}
+	};
 
-    return (
-        <div className={'homepage ' + (localStorage.getItem('loggedIn') == 'true' ? (localStorage.getItem('role') == '[admin]' ? 'admin-background' : 'customer-background') : 'login-register-background')}>
-            <div>
-    <div>
-        {localStorage.getItem('loggedIn') === 'true' && (
-            <div className="navigation-container">
-                <ul className="navigation-list">
-                    {localStorage.getItem('role') === '[admin]' && (
-                        <button className="nav-link yellow" onClick={() => navigate('/admin')}>
-                            Home
-                        </button>
-                    )}
-                    {localStorage.getItem('role') === '[customer]' && (
-                        <>
-                            <button className="yellow" onClick={() => navigate('/customer')}>
-                                Home
-                            </button>
-                            <button className="yellow" onClick={() => navigate('/addCustomerDetails')}>
-                                Profile
-                            </button>
-                        </>
-                    )}
-                </ul>
-                <button className="btn logout-btn red" onClick={logout}>
-                    Log out
-                </button>
-            </div>
-        )}
-    </div>
-    </div>
-           
-           <Routes>
-           <Route path="/addCustomerDetails" element={<AddCustomerDetails/>}></Route>
-           <Route path="/menu" element={<Menu />}></Route>
-           <Route path="/register" element={<Register />}></Route>
-           <Route path="/" element={<Login />}></Route>
-           <Route path="/addCategories" element={<AddCategories/>}></Route>
-           <Route path="/cart" element={<Cart />}></Route>
-           <Route path="/placeorder" element={<PlaceOrder />}></Route>
-           <Route path="/orders" element={<Orders />}></Route>           
-           <Route path="/orders/id" element={<MyOrders />}></Route>
-           <Route path="/orders/cancel/:oid" element={<CancelOrder />}></Route>
-           <Route path="/orders/updateStatus/:oid" element={<UpdateOrderStatus />}></Route>
-           <Route path="/orderitems/:oid" element={<OrderedFoodItems />}></Route>
-           <Route path="/admin" element={<Admin/>}></Route>
-           <Route path="/addPizza/:categoryId/:pizzaId" element={<AddPizza/>}></Route>
-           <Route path="/pizza" element={<Pizza/>}></Route>
-           <Route path="/addTopping" element={<AddToppings/>}></Route>
-           <Route path="/customer" element={<CustomerHome/>}></Route>
-           <Route path="/adminupdateorders" element={<AdminUpadateOrders/>}></Route>
-           <Route path="/adminupdateorderstatus" element={<AdminUpdateOrderStatus/>}></Route>
-           <Route path="/:id/toppings" element={<Toppings/>}></Route>
-           <Route path="/addfeedback/:pizzaId" element={<AddFeedBack/>}></Route>
-           <Route path="/feedback/:pizzaId" element={<FeedBack/>}></Route>
-           <Route path="/CustomerSearch" element={<CustomerSearch/>}></Route>
-           <Route path="/allFeedbacks" element={<FeedbackAdmin/>}></Route>
-           <Route path="/:id/toppings" element={<Toppings/>}></Route>
+	return (
+		<div
+			className={
+				"homepage " +
+				(localStorage.getItem("loggedIn") == "true"
+					? localStorage.getItem("role") == "[admin]"
+						? "admin-background"
+						: "customer-background"
+					: "login--background")
+			}
+		>
+			<div>
+				<div>
+					{localStorage.getItem("loggedIn") === "true" && (
+						<div className='navigation-container'>
+							<ul className='navigation-list'>
+								{localStorage.getItem("role") === "[admin]" && (
+									<button
+										className='nav-link yellow'
+										onClick={() => navigate("/admin")}
+									>
+										Home
+									</button>
+								)}
+								{localStorage.getItem("role") === "[customer]" && (
+									<>
+										<button
+											className='yellow'
+											onClick={() => navigate("/customer")}
+										>
+											Home
+										</button>
+										<button
+											className='yellow'
+											onClick={() => navigate("/addCustomerDetails")}
+										>
+											Profile
+										</button>
+									</>
+								)}
+							</ul>
+							<button className='btn logout-btn red' onClick={logout}>
+								Log out
+							</button>
+						</div>
+					)}
+				</div>
+			</div>
 
-
-
-       </Routes>
-       </div>
-);
+			<Routes>
+				<Route
+					path='/addCustomerDetails'
+					element={<AddCustomerDetails />}
+				></Route>
+				<Route path='/menu' element={<Menu />}></Route>
+				<Route path='/register' element={<Register />}></Route>
+				<Route path='/' element={<Login />}></Route>
+				<Route path='/addCategories' element={<AddCategories />}></Route>
+				<Route path='/cart' element={<Cart />}></Route>
+				<Route path='/placeorder' element={<PlaceOrder />}></Route>
+				<Route path='/orders' element={<Orders />}></Route>
+				<Route path='/orders/id' element={<MyOrders />}></Route>
+				<Route path='/orders/cancel/:oid' element={<CancelOrder />}></Route>
+				<Route
+					path='/orders/updateStatus/:oid'
+					element={<UpdateOrderStatus />}
+				></Route>
+				<Route path='/orderitems/:oid' element={<OrderedFoodItems />}></Route>
+				<Route path='/admin' element={<Admin />}></Route>
+				<Route
+					path='/addPizza/:categoryId/:pizzaId'
+					element={<AddPizza />}
+				></Route>
+				<Route path='/pizza' element={<Pizza />}></Route>
+				<Route path='/addTopping' element={<AddToppings />}></Route>
+				<Route path='/customer' element={<CustomerHome />}></Route>
+				<Route
+					path='/adminupdateorders'
+					element={<AdminUpadateOrders />}
+				></Route>
+				<Route
+					path='/adminupdateorderstatus'
+					element={<AdminUpdateOrderStatus />}
+				></Route>
+				<Route path='/:id/toppings' element={<Toppings />}></Route>
+				<Route path='/addfeedback/:pizzaId' element={<AddFeedBack />}></Route>
+				<Route path='/feedback/:pizzaId' element={<FeedBack />}></Route>
+				<Route path='/CustomerSearch' element={<CustomerSearch />}></Route>
+				<Route path='/allFeedbacks' element={<FeedbackAdmin />}></Route>
+				<Route path='/:id/toppings' element={<Toppings />}></Route>
+			</Routes>
+		</div>
+	);
 }
 export default Home;
--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -1,29 +1,55 @@
 import { useEffect, useState } from "react";
 import ApiService from "../service/ApiService";
 
-function Pizza(){
-    let [allcategories,setallCategories]=useState([]);
-    let apiService=new ApiService();
-     useEffect(() => {
-      apiService.getAllCategory().then((res) => {
-        setallCategories(res.data);
-        
-      },
-        (err) => { console.log("Error " + err) }
-      );
-    }, []);
+function Pizza() {
+	let [allcategories, setallCategories] = useState([]);
+	let apiService = new ApiService();
+	useEffect(() => {
+		apiService.getAllCategory().then(
+			(res) => {
+				setallCategories(res.data);
+			},
+			(err) => {
+				console.log("Error " + err);
+			},
+		);
+	}, []);
 
-    return <div className="row">
-{allcategories.map((category) => (
-          <div key={category.categoryId} className="cat col-md-4 mb-4">
-            <div className="cat card h-100">
-              <div className="cat card-body">
-                <h5 className="cat card-title">{category.categoryName}</h5>
-              </div>
-              <a href={'/addPizza/'+category.categoryId+'/0'}>Add Pizza</a>
-            </div>
-          </div>
-        ))}
-        </div>
+	return (
+		<div className='row'>
+			{allcategories.map((category) => (
+				<div key={category.categoryId} className='cat col-md-4 mb-4'>
+					<div className='card'>
+						<img
+							src='https://mdbcdn.b-cdn.net/img/new/standard/nature/184.webp'
+							class='card-img-top'
+							alt='Fissure in Sandstone'
+						/>
+
+						<div className='card-body'>
+							<h5 className='card-text'>{category.categoryName}</h5>
+						</div>
+						<div
+							className='w-100'
+							style={{ display: "flex", justifyContent: "center" }}
+						>
+							<button
+								type='button'
+								class='btn btn-secondary '
+								data-mdb-ripple-init
+							>
+								<a
+									href={"/addPizza/" + category.categoryId + "/0"}
+									style={{ all: "unset" }}
+								>
+									+ Add Pizza
+								</a>
+							</button>
+						</div>
+					</div>
+				</div>
+			))}
+		</div>
+	);
 }
 export default Pizza;
